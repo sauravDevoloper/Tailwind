10b62ceecb2cd3d5aa264195da943ed40acd703d
Saki9891 <saikirankollimalla@gmail.com>
2024-07-27 11:46:09 +0530

    git

--- src/TailwindCss/Components/HeroSection.jsx
+++ src/TailwindCss/Components/HeroSection.jsx
@@ -16,7 +16,7 @@ const HeroSection = () => {
       </div>
     </div>
     <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
-      <img className="object-cover object-center rounded" alt="hero" src="/public/assets/child.png"/>
+      <img className="object-cover object-center rounded" alt="hero" src="/public/assets/icecream.png"/>
     </div>
   </div>
 </section>
